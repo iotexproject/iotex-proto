6a5ec13d68ffc71583bb5b9594d034c4acb08f6f
dustinxie <dustin.xie@iotex.io>
2019-05-17 10:18:54 -0700

    Add gas fee to ActionInfo and new API SendSignedActionBytes() (#7)

--- golang/iotexapi/api.pb.gw.go
+++ golang/iotexapi/api.pb.gw.go
@@ -142,6 +142,33 @@ func request_APIService_GetActionsByAddress_0(ctx context.Context, marshaler run
 
 }
 
+func request_APIService_SendSignedActionBytes_0(ctx context.Context, marshaler runtime.Marshaler, client APIServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq SendSignedActionBytesRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["signedActionBytes"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "signedActionBytes")
+	}
+
+	protoReq.SignedActionBytes, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "signedActionBytes", err)
+	}
+
+	msg, err := client.SendSignedActionBytes(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterAPIServiceHandlerFromEndpoint is same as RegisterAPIServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterAPIServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -260,6 +287,26 @@ func RegisterAPIServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
+	mux.Handle("POST", pattern_APIService_SendSignedActionBytes_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_APIService_SendSignedActionBytes_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_APIService_SendSignedActionBytes_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -271,6 +318,8 @@ var (
 	pattern_APIService_SendAction_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "actions"}, ""))
 
 	pattern_APIService_GetActionsByAddress_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"v1", "actions", "addr", "address"}, ""))
+
+	pattern_APIService_SendSignedActionBytes_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "actionbytes", "signedActionBytes"}, ""))
 )
 
 var (
@@ -281,4 +330,6 @@ var (
 	forward_APIService_SendAction_0 = runtime.ForwardResponseMessage
 
 	forward_APIService_GetActionsByAddress_0 = runtime.ForwardResponseMessage
+
+	forward_APIService_SendSignedActionBytes_0 = runtime.ForwardResponseMessage
 )
