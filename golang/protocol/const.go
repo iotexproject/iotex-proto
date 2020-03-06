fd0de74603830e2470f50704c78a2a4211f8bc24
Yutong Pei <yutong@iotex.io>
2020-03-05 16:28:11 -0800

    add state read for staking

--- golang/protocol/const.go
+++ golang/protocol/const.go
@@ -1,8 +1,17 @@
 package protocol
 
 const (
-	RewardingProtocolID = "rewarding"
+	RewardingProtocolID            = "rewarding"
 	ReadAvailableBalanceMethodName = "AvailableBalance"
-	ReadTotalBalanceMethodName = "TotalBalance"
+	ReadTotalBalanceMethodName     = "TotalBalance"
 	ReadUnclaimedBalanceMethodName = "UnclaimedBalance"
+)
+
+const (
+	StakingProtcolID                            = "staking"
+	ReadStakingVoteBucketsMethodName            = "getBuckets"
+	ReadStakingVoteBucketsByVoterMethodName     = "getBucketsByVoter"
+	ReadStakingVoteBucketsByCandidateMethodName = "getBucketsByCandidate"
+	ReadStakingCandidatesMethodName             = "getCandidates"
+	ReadStakingCandidatesByNameMethodName       = "getCandidatesByName"
 )
