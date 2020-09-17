80f7c838296d4681e96dbc639316361755591282
Dustin Xie <dustin.xie@iotex.io>
2020-09-17 09:26:41 -0700

    add BlockStore message

--- golang/iotextypes/blockchain.pb.go
+++ golang/iotextypes/blockchain.pb.go
@@ -834,6 +834,108 @@ func (x *AccountMeta) GetIsContract() bool {
 	return false
 }
 
+type BlockStore struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Block    *Block     `protobuf:"bytes,1,opt,name=block,proto3" json:"block,omitempty"`
+	Receipts []*Receipt `protobuf:"bytes,2,rep,name=receipts,proto3" json:"receipts,omitempty"`
+}
+
+func (x *BlockStore) Reset() {
+	*x = BlockStore{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_proto_types_blockchain_proto_msgTypes[11]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *BlockStore) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*BlockStore) ProtoMessage() {}
+
+func (x *BlockStore) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_types_blockchain_proto_msgTypes[11]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use BlockStore.ProtoReflect.Descriptor instead.
+func (*BlockStore) Descriptor() ([]byte, []int) {
+	return file_proto_types_blockchain_proto_rawDescGZIP(), []int{11}
+}
+
+func (x *BlockStore) GetBlock() *Block {
+	if x != nil {
+		return x.Block
+	}
+	return nil
+}
+
+func (x *BlockStore) GetReceipts() []*Receipt {
+	if x != nil {
+		return x.Receipts
+	}
+	return nil
+}
+
+type BlockStores struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	BlockStores []*BlockStore `protobuf:"bytes,1,rep,name=blockStores,proto3" json:"blockStores,omitempty"`
+}
+
+func (x *BlockStores) Reset() {
+	*x = BlockStores{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_proto_types_blockchain_proto_msgTypes[12]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *BlockStores) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*BlockStores) ProtoMessage() {}
+
+func (x *BlockStores) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_types_blockchain_proto_msgTypes[12]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use BlockStores.ProtoReflect.Descriptor instead.
+func (*BlockStores) Descriptor() ([]byte, []int) {
+	return file_proto_types_blockchain_proto_rawDescGZIP(), []int{12}
+}
+
+func (x *BlockStores) GetBlockStores() []*BlockStore {
+	if x != nil {
+		return x.BlockStores
+	}
+	return nil
+}
+
 var File_proto_types_blockchain_proto protoreflect.FileDescriptor
 
 var file_proto_types_blockchain_proto_rawDesc = []byte{
@@ -956,13 +1058,24 @@ var file_proto_types_blockchain_proto_rawDesc = []byte{
 	0x18, 0x05, 0x20, 0x01, 0x28, 0x04, 0x52, 0x0a, 0x6e, 0x75, 0x6d, 0x41, 0x63, 0x74, 0x69, 0x6f,
 	0x6e, 0x73, 0x12, 0x1e, 0x0a, 0x0a, 0x69, 0x73, 0x43, 0x6f, 0x6e, 0x74, 0x72, 0x61, 0x63, 0x74,
 	0x18, 0x06, 0x20, 0x01, 0x28, 0x08, 0x52, 0x0a, 0x69, 0x73, 0x43, 0x6f, 0x6e, 0x74, 0x72, 0x61,
-	0x63, 0x74, 0x42, 0x5d, 0x0a, 0x22, 0x63, 0x6f, 0x6d, 0x2e, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62,
-	0x2e, 0x69, 0x6f, 0x74, 0x65, 0x78, 0x70, 0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74, 0x2e, 0x67, 0x72,
-	0x70, 0x63, 0x2e, 0x74, 0x79, 0x70, 0x65, 0x73, 0x50, 0x01, 0x5a, 0x35, 0x67, 0x69, 0x74, 0x68,
-	0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x69, 0x6f, 0x74, 0x65, 0x78, 0x70, 0x72, 0x6f, 0x6a,
-	0x65, 0x63, 0x74, 0x2f, 0x69, 0x6f, 0x74, 0x65, 0x78, 0x2d, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f,
-	0x67, 0x6f, 0x6c, 0x61, 0x6e, 0x67, 0x2f, 0x69, 0x6f, 0x74, 0x65, 0x78, 0x74, 0x79, 0x70, 0x65,
-	0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x63, 0x74, 0x22, 0x66, 0x0a, 0x0a, 0x42, 0x6c, 0x6f, 0x63, 0x6b, 0x53, 0x74, 0x6f, 0x72, 0x65,
+	0x12, 0x27, 0x0a, 0x05, 0x62, 0x6c, 0x6f, 0x63, 0x6b, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32,
+	0x11, 0x2e, 0x69, 0x6f, 0x74, 0x65, 0x78, 0x74, 0x79, 0x70, 0x65, 0x73, 0x2e, 0x42, 0x6c, 0x6f,
+	0x63, 0x6b, 0x52, 0x05, 0x62, 0x6c, 0x6f, 0x63, 0x6b, 0x12, 0x2f, 0x0a, 0x08, 0x72, 0x65, 0x63,
+	0x65, 0x69, 0x70, 0x74, 0x73, 0x18, 0x02, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x13, 0x2e, 0x69, 0x6f,
+	0x74, 0x65, 0x78, 0x74, 0x79, 0x70, 0x65, 0x73, 0x2e, 0x52, 0x65, 0x63, 0x65, 0x69, 0x70, 0x74,
+	0x52, 0x08, 0x72, 0x65, 0x63, 0x65, 0x69, 0x70, 0x74, 0x73, 0x22, 0x47, 0x0a, 0x0b, 0x42, 0x6c,
+	0x6f, 0x63, 0x6b, 0x53, 0x74, 0x6f, 0x72, 0x65, 0x73, 0x12, 0x38, 0x0a, 0x0b, 0x62, 0x6c, 0x6f,
+	0x63, 0x6b, 0x53, 0x74, 0x6f, 0x72, 0x65, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x16,
+	0x2e, 0x69, 0x6f, 0x74, 0x65, 0x78, 0x74, 0x79, 0x70, 0x65, 0x73, 0x2e, 0x42, 0x6c, 0x6f, 0x63,
+	0x6b, 0x53, 0x74, 0x6f, 0x72, 0x65, 0x52, 0x0b, 0x62, 0x6c, 0x6f, 0x63, 0x6b, 0x53, 0x74, 0x6f,
+	0x72, 0x65, 0x73, 0x42, 0x5d, 0x0a, 0x22, 0x63, 0x6f, 0x6d, 0x2e, 0x67, 0x69, 0x74, 0x68, 0x75,
+	0x62, 0x2e, 0x69, 0x6f, 0x74, 0x65, 0x78, 0x70, 0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74, 0x2e, 0x67,
+	0x72, 0x70, 0x63, 0x2e, 0x74, 0x79, 0x70, 0x65, 0x73, 0x50, 0x01, 0x5a, 0x35, 0x67, 0x69, 0x74,
+	0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x69, 0x6f, 0x74, 0x65, 0x78, 0x70, 0x72, 0x6f,
+	0x6a, 0x65, 0x63, 0x74, 0x2f, 0x69, 0x6f, 0x74, 0x65, 0x78, 0x2d, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x2f, 0x67, 0x6f, 0x6c, 0x61, 0x6e, 0x67, 0x2f, 0x69, 0x6f, 0x74, 0x65, 0x78, 0x74, 0x79, 0x70,
+	0x65, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -977,7 +1090,7 @@ func file_proto_types_blockchain_proto_rawDescGZIP() []byte {
 	return file_proto_types_blockchain_proto_rawDescData
 }
 
-var file_proto_types_blockchain_proto_msgTypes = make([]protoimpl.MessageInfo, 11)
+var file_proto_types_blockchain_proto_msgTypes = make([]protoimpl.MessageInfo, 13)
 var file_proto_types_blockchain_proto_goTypes = []interface{}{
 	(*BlockHeader)(nil),         // 0: iotextypes.BlockHeader
 	(*BlockHeaderCore)(nil),     // 1: iotextypes.BlockHeaderCore
@@ -990,28 +1103,33 @@ var file_proto_types_blockchain_proto_goTypes = []interface{}{
 	(*BlockMeta)(nil),           // 8: iotextypes.BlockMeta
 	(*BlockIdentifier)(nil),     // 9: iotextypes.BlockIdentifier
 	(*AccountMeta)(nil),         // 10: iotextypes.AccountMeta
-	(*timestamp.Timestamp)(nil), // 11: google.protobuf.Timestamp
-	(*Endorsement)(nil),         // 12: iotextypes.Endorsement
-	(*Action)(nil),              // 13: iotextypes.Action
-	(*Receipt)(nil),             // 14: iotextypes.Receipt
+	(*BlockStore)(nil),          // 11: iotextypes.BlockStore
+	(*BlockStores)(nil),         // 12: iotextypes.BlockStores
+	(*timestamp.Timestamp)(nil), // 13: google.protobuf.Timestamp
+	(*Endorsement)(nil),         // 14: iotextypes.Endorsement
+	(*Action)(nil),              // 15: iotextypes.Action
+	(*Receipt)(nil),             // 16: iotextypes.Receipt
 }
 var file_proto_types_blockchain_proto_depIdxs = []int32{
 	1,  // 0: iotextypes.BlockHeader.core:type_name -> iotextypes.BlockHeaderCore
-	11, // 1: iotextypes.BlockHeaderCore.timestamp:type_name -> google.protobuf.Timestamp
-	12, // 2: iotextypes.BlockFooter.endorsements:type_name -> iotextypes.Endorsement
-	11, // 3: iotextypes.BlockFooter.timestamp:type_name -> google.protobuf.Timestamp
-	13, // 4: iotextypes.BlockBody.actions:type_name -> iotextypes.Action
+	13, // 1: iotextypes.BlockHeaderCore.timestamp:type_name -> google.protobuf.Timestamp
+	14, // 2: iotextypes.BlockFooter.endorsements:type_name -> iotextypes.Endorsement
+	13, // 3: iotextypes.BlockFooter.timestamp:type_name -> google.protobuf.Timestamp
+	15, // 4: iotextypes.BlockBody.actions:type_name -> iotextypes.Action
 	0,  // 5: iotextypes.Block.header:type_name -> iotextypes.BlockHeader
 	3,  // 6: iotextypes.Block.body:type_name -> iotextypes.BlockBody
 	2,  // 7: iotextypes.Block.footer:type_name -> iotextypes.BlockFooter
-	14, // 8: iotextypes.Receipts.receipts:type_name -> iotextypes.Receipt
+	16, // 8: iotextypes.Receipts.receipts:type_name -> iotextypes.Receipt
 	6,  // 9: iotextypes.ChainMeta.epoch:type_name -> iotextypes.EpochData
-	11, // 10: iotextypes.BlockMeta.timestamp:type_name -> google.protobuf.Timestamp
-	11, // [11:11] is the sub-list for method output_type
-	11, // [11:11] is the sub-list for method input_type
-	11, // [11:11] is the sub-list for extension type_name
-	11, // [11:11] is the sub-list for extension extendee
-	0,  // [0:11] is the sub-list for field type_name
+	13, // 10: iotextypes.BlockMeta.timestamp:type_name -> google.protobuf.Timestamp
+	4,  // 11: iotextypes.BlockStore.block:type_name -> iotextypes.Block
+	16, // 12: iotextypes.BlockStore.receipts:type_name -> iotextypes.Receipt
+	11, // 13: iotextypes.BlockStores.blockStores:type_name -> iotextypes.BlockStore
+	14, // [14:14] is the sub-list for method output_type
+	14, // [14:14] is the sub-list for method input_type
+	14, // [14:14] is the sub-list for extension type_name
+	14, // [14:14] is the sub-list for extension extendee
+	0,  // [0:14] is the sub-list for field type_name
 }
 
 func init() { file_proto_types_blockchain_proto_init() }
@@ -1154,6 +1272,30 @@ func file_proto_types_blockchain_proto_init() {
 				return nil
 			}
 		}
+		file_proto_types_blockchain_proto_msgTypes[11].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*BlockStore); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_proto_types_blockchain_proto_msgTypes[12].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*BlockStores); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -1161,7 +1303,7 @@ func file_proto_types_blockchain_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_proto_types_blockchain_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   11,
+			NumMessages:   13,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
